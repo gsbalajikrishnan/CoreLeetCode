002f9296b816c3c4cb653ac7aa1624a8cd3b5a20
gsbalajikrishnan <gsbalajikrishnan@gmail.com>
2026-05-17 14:24:16 +0530

    updated th code

--- Bitwise/unset_bits.c
+++ Bitwise/unset_bits.c
@@ -5,9 +5,9 @@ Unset bits after specific bit
 int main()
 {
     int value = 0xFF; //1111 1111
-    int bit = 5;
+    int bit = 6;
     value = value & ((1<<bit)-1);   
-    printf("%x",value); //0000 0111
+    printf("%x",value); //0001 1111
     return 0;
     
 }
